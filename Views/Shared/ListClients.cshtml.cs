408d8dd94a6e72bf5fb2e167fe734e70b847ab27
apollw <apollw@gmail.com>
2024-09-03 04:23:55 -0300

    feat: client-controller updated

---
+++ Views/Shared/ListClients.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDigital.Views.Shared
+{
+    public class ListClientsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
